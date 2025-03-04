8303b84e4f30b3fec02a62071538cd9d8a2f3daf
leartoler <fabiangg@protonmail.ch>
2025-03-04 10:02:01 -0500

    lastupdate

---
+++ v0-00-01_20250228/Assets/ARObjectSpawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using System.Collections.Generic;
+
+public class ARObjectSpawner : MonoBehaviour
+{
+    public GameObject objectPrefab;  
+    private ARRaycastManager raycastManager;  
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    void Start()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (raycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
+            {
+                Pose hitPose = hits[0].pose;
+                Instantiate(objectPrefab, hitPose.position, hitPose.rotation);
+            }
+        }
+    }
+}
+   
